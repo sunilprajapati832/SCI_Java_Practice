884b1b1385798af7a220ad74e271d108095b0ac8
Sunil Prajapati <sunilp832@gmail.com>
2026-04-19 13:08:57 +0530

    Constructor Called

--- src/Practice.java
+++ src/Practice.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Practice {
+class N
+{
+    static int a;
+    N()
+    {
+     System.out.println("Constructor Called");
+    }
+}
+public class Practice {
+    public static void main(String[] args)
+    {
+        System.out.println("Default Value of a is "+N.a);
+        N obj1 = new N();
+        obj1.a = 100;
+
+        N obj2 = new N();
+        System.out.println("Updated Value of a is " + obj2.a);
+    }
+
 }
--- src/Questions.java
+++ src/Questions.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Questions {
+// give out of this code ? (RPSC Programmer 2024)
+class A
+{
+    int a = 10;
+    void M1()
+    {
+        System.out.print("class A M1 called");
+    }
+}
+
+class B extends A
+{
+    int a = 20;
+    void M1()
+    {
+        System.out.print("class B M1 called");
+    }
+}
+
+public class Questions {
+    public static void main(String[] args)
+    {
+        A obj = new B();
+        System.out.println(obj.a);
+        obj.M1();
+    }
 }
